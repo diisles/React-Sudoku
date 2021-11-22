0b670b21b9ac1a7ad7c2c1786b67122707ca1801
David Isles <davidisles@Davids-MacBook-Pro.local>
2021-11-22 12:27:33 -0800

    Fixed Card Size

---
+++ src/components/grid/block/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react'
+
+interface IProps {
+    colIndex: number
+    rowIndex: number
+}
+
+const Block: FC<IProps> = ({ colIndex, rowIndex }) => {
+    return ( 
+    <div data-cy="block"> 
+    |{colIndex}{rowIndex}| 
+    </div>
+   )
+}
+
+export default Block
